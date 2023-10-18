4a6e52df245193f25b45fed2c65bcfec6597b499
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2023-10-18 20:31:16 +0200

    Installed font awoesome icons.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Font Awesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
+/* end Font Awesome */
+
 const app = createApp(App)
 
 app.use(router)
