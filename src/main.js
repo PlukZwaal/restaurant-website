ce211c4467080dca23e41b107ff66f8dc6ae97cf
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2023-10-18 21:07:16 +0200

    Removed fontawesome from project.

--- src/main.js
+++ src/main.js
@@ -4,15 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-/* Font Awesome */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-library.add(faUserSecret)
-/* end Font Awesome */
-
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
